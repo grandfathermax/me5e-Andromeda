df9bd892765d65212dcfe4a8e8aa803a7bd0a9df
grandfathermax <45001991+grandfathermax@users.noreply.github.com>
2025-03-02 20:34:16 +0000

    Languages

--- src/me5e-andromeda.js
+++ src/me5e-andromeda.js
@@ -1,3 +1,44 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.languages.standard.children.angaran = "Angaran";
+  CONFIG.DND5E.languages.standard.children.asari = "Asari";
+  CONFIG.DND5E.languages.standard.children.krogan = "Krogan";
+  CONFIG.DND5E.languages.standard.children.salarian = "Salarian";
+  CONFIG.DND5E.languages.standard.children.terran = "Terran";	
+  CONFIG.DND5E.languages.standard.children.turian = "Turian";	
+	//Remove traditional common fantasy languages
+	delete  CONFIG.DND5E.languages.standard.children.draconic;	
+	delete  CONFIG.DND5E.languages.standard.children.dwarvish;
+	delete  CONFIG.DND5E.languages.standard.children.elvish;
+	delete  CONFIG.DND5E.languages.standard.children.giant;
+	delete  CONFIG.DND5E.languages.standard.children.gnomish;
+	delete  CONFIG.DND5E.languages.standard.children.goblin;
+	delete  CONFIG.DND5E.languages.standard.children.halfling;
+	delete  CONFIG.DND5E.languages.standard.children.orc;
+        delete  CONFIG.DND5E.languages.standard.children.common;
+        delete  CONFIG.DND5E.languages.standard.children.sign;
+
+	//Remove traditional rare fantasy languages
+	delete CONFIG.DND5E.languages.exotic.children.aarakocra;
+        delete CONFIG.DND5E.languages.exotic.children.abyssal;
+        delete CONFIG.DND5E.languages.exotic.children.celestial;
+        delete CONFIG.DND5E.languages.exotic.children.deep;
+        delete CONFIG.DND5E.languages.exotic.children.druidic;
+        delete CONFIG.DND5E.languages.exotic.children.gnoll;
+        delete CONFIG.DND5E.languages.exotic.children.infernal;
+        delete CONFIG.DND5E.languages.exotic.children.primordial.children.aquan;
+        delete CONFIG.DND5E.languages.exotic.children.primordial.children.auran;
+        delete CONFIG.DND5E.languages.exotic.children.primordial.children.ignan;
+        delete CONFIG.DND5E.languages.exotic.children.primordial.children.terran;	
+        delete CONFIG.DND5E.languages.exotic.children.primordial;
+
+
+        delete CONFIG.DND5E.languages.exotic.children.sylvan;
+        delete CONFIG.DND5E.languages.exotic.children.undercommon;	
+        delete CONFIG.DND5E.languages.exotic.children.cant;
+
+
+});
+
 //Changing out deprecated 5e skills to their replacements
 dnd5e.config.skills.arc = {label: "Electronics" , ability: "int"};
 dnd5e.config.skills.nat = {label: "Engineering" , ability: "int"};
@@ -36,6 +77,8 @@ function extendActorData() {
 
     return prep.call(this);
 }
+
+
 
 //Changing "schools" of magic
 dnd5e.config.spellSchools['abj'] = 'Biotics';
@@ -96,6 +139,7 @@ dnd5e.config.toolProficiencies['sshe'] = 'Starship Systems (Helm)';
 dnd5e.config.toolProficiencies['sssc'] = 'Starship Systems (SSC)';
 dnd5e.config.toolProficiencies['ssew'] = 'Starship Systems (EWS)';
 dnd5e.config.toolProficiencies['sswp'] = 'Starship Systems (Weapons)';
+dnd5e.config.toolProficiencies['ssnv'] = 'Starship Systems (Navigation)';
 dnd5e.config.toolProficiencies['tail'] = "Tailor's Tools";
 dnd5e.config.toolProficiencies['tink'] = "Tinker's Tools";
 dnd5e.config.toolProficiencies['wswb'] = "Weaponsmith's Workbench";
@@ -182,66 +226,6 @@ Items.registerSheet('DND5E', ME5eItemSheet, {
 Hooks.on('renderActorSheet', (app, html, data) => {
     const healthdiv = html
         .find('header.sheet-header')
-        .find('ul.attributes.flexrow');
-    const flags = data.actor.flags.me5e || {};
-    healthdiv.prepend(`
-		  		<li class="attribute shields">
-                    <h4 class="attribute-name box-title">Shields</h4>
-                    <div class="attribute-value multiple">
-                        <input name="flags.me5e.shields" type="text" value="${flags.shields ?? 0}" data-dtype="Number" placeholder="5"/>
-                        <span class="sep"> / </span>
-                        <input name="flags.me5e.shieldsMax" type="text" value="${flags.shieldsMax ?? 0}" data-dtype="Number" placeholder="5"/>
-                    </div>
-                    <footer class="attribute-footer">
-                        <input name="flags.me5e.shieldsRegen" type="text" class="shieldsRegen" placeholder="Shield Regen." value="${flags.shieldsRegen ?? 0}" data-dtype="Number"/>
-                    </footer>
-                </li>
-	  `);
-
-
-    const col = html
-        .find('section.center-pane.flexcol')
-        .find('ul.attributes.flexrow');
-    const counters = html.find('div.counters');
-    counters.append(`
-	<div class="counter flexrow indoctrination">
-    <h4> Indoctrination </h4>
-    <div class="counter-value">
-      <input type="text" name="flags.me5e.indoctrination" placeholder="0" value="${
-          flags.indoctrination ?? 0
-      }" data-dtype="Number"/>
-    </div>
-	</div>
-	
-	`);
-    col.prepend(`
-	
-    <li class="attribute reputation">
-        <h4 class="attribute-name box-title">Reputation</h4>
-        <div class="attribute-value attributable">
-        <input type="text" name="flags.me5e.paragon" placeholder="0" value="${
-            flags.paragon ?? 0
-        }" data-dtype="Number"/>
-            <span class="sep"> / </span>
-            <input type="text" name="flags.me5e.renegade" placeholder="0" value="${
-                flags.renegade ?? 0
-            }" data-dtype="Number"/>
-            
-        </div>
-
-
-        <footer class="attribute-footer">
-            <span class="spell-dc" >Paragon</span>
-            <span class="sep"> / </span>
-            <span class="spell-dc" >Renegade</span>
-        </footer>
-            
-        
-    </li>
-	
-	`);
-});
-heet-header')
         .find('ul.attributes.flexrow');
     const flags = data.actor.flags.me5e || {};
     healthdiv.prepend(`
